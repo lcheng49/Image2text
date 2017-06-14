1669194646fd04edffa51bbc596f377e73ecd503
lcheng49 <lukcheng@umich.edu>
2017-06-14 15:41:38 -0400

    functor

--- functor.js
+++ functor.js
@@ -11,26 +11,27 @@ function compy(a, b){
 }
 
 function compx(a,b){
-	if(((a.y_start + a.y_dim)/2) - ((b.y_start + b.y_dim)/2)) > 5){
-	return 0;
+	//if((((a.y_start + a.y_dim)/2) - ((b.y_start + b.y_dim)/2)) > 5){
+	//return 0;
+	//}
+	//else{
+	if(a.x_start < b.x_start){
+		return -1;
 	}
-	else{
-		if(a.x_start < b.x_start){
-			return 1;
-		}
-		if(a.xstart > b.x_start){
-			return -1;
-		}
-		return 0;
+	else if(a.xstart > b.x_start){
+		return 1;
+	}
+	return 0;
+	//return 0;
 	}	
+//}
 
-}
 
 function distForm(a,b){
 	total = 0;
 	for(i = 0; i < 1000; ++i){
-		new = Math.pow((a[i] - b[i]), 2);
-		total += new;
+		newVal = Math.pow((a[i] - b[i]), 2);
+		total += newVal;
 	}
-	return total;
+	return Math.sqrt(total);
 }
