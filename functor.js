c52d9b39b192255581311a1aadf788c481683300
mXaln <murmax82@gmail.com>
2017-06-14 15:41:24 -0400

    first working version

--- functor.js
+++ functor.js
@@ -1,36 +1,28 @@
-function compy(a, b){
-	if(((a.y_start + a.y_dim)/2) < ((b.y_start + b.y_dim)/2)){
-		return 1;
-	}
-	else if (((a.y_start + a.y_dim)/2) > ((b.y_start + b.y_dim)/2)){
-		return -1;
-	}
-	else{
-		return 0;
-	}
+function compy(a, b) {
+    if (((a.y_start + a.y_dim) / 2) < ((b.y_start + b.y_dim) / 2)) {
+        return 1;
+    } else if (((a.y_start + a.y_dim) / 2) > ((b.y_start + b.y_dim) / 2)) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
-function compx(a,b){
-	if(((a.y_start + a.y_dim)/2) - ((b.y_start + b.y_dim)/2)) > 5){
-	return 0;
-	}
-	else{
-		if(a.x_start < b.x_start){
-			return 1;
-		}
-		if(a.xstart > b.x_start){
-			return -1;
-		}
-		return 0;
-	}	
-
+function compx(a, b) {
+    if (a.x_start > b.x_start) {
+        return 1;
+    }
+    if (a.xstart < b.x_start) {
+        return -1;
+    }
+    return 0;
 }
 
-function distForm(a,b){
-	total = 0;
-	for(i = 0; i < 1000; ++i){
-		new = Math.pow((a[i] - b[i]), 2);
-		total += new;
-	}
-	return total;
+function distForm(a, b) {
+    total = 0;
+    for (i = 0; i < 1000; ++i) {
+        newVar = Math.pow((a[i] - b[i]), 2);
+        total += newVar;
+    }
+    return total;
 }
