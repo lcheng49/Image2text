6ee72f43ef8f56f589a869731ef2ab5e6b969c91
lcheng49 <lukcheng@umich.edu>
2017-06-15 11:13:19 -0400

    Working version for now lul

--- functor.js
+++ functor.js
@@ -1,28 +1,85 @@
-function compy(a, b) {
-    if (((a.y_start + a.y_dim) / 2) < ((b.y_start + b.y_dim) / 2)) {
+/*function compy(a, b) {
+    if (((a.y_start + a.y_dim) / 2.0) < ((b.y_start + b.y_dim) / 2.0)) {
         return 1;
-    } else if (((a.y_start + a.y_dim) / 2) > ((b.y_start + b.y_dim) / 2)) {
+    } 
+    if (((a.y_start + a.y_dim) / 2.0) > ((b.y_start + b.y_dim) / 2.0)) {
+        return -1;
+    } 
+    return 0;
+}*/
+/*function compg(a,b){
+    if(a.y_start < b.y_start){
+        return -1;
+    }
+    if(a.y_start > b.y_start){
+        return 1;
+    }
+    return 0;
+}*/
+/*function compa(a,b){
+    return (a.y_start - b.y_start);
+}*/
+
+/*function compx(a, b) {
+    if((((a.y_start + a.y_dim)/2) - ((b.y_start + b.y_dim)/2)) > 10){
+    	return -1;
+    }
+    else{
+    	if (a.x_start > b.x_start) {
+        	return 1;
+    	}
+    	if (a.xstart < b.x_start) {
+        	return -1;
+    	}
+		return 0;
+	}
+}*/
+function compp(a,b){
+    console.log("a "  + a.y_start + " b " + b.y_start);
+    console.log(typeof(a.x_start));
+    if(a.y_start > b.y_start){
+        console.log("chose 1");
+        return 1;
+    }
+    else if(a.y_start < b.y_start){
+        console.log("chose -1");
         return -1;
     } else {
+        console.log("chose 0");
         return 0;
     }
 }
-
-function compx(a, b) {
-    //if((((a.y_start + a.y_dim)/2) - ((b.y_start + b.y_dim)/2)) > 5){
-    //return 0;
-    //}
-    //else{
-    if (a.x_start > b.x_start) {
-        return 1;
+function comp(a,b){
+    if(Math.abs(a.y_start - b.y_start) < 30){
+        if (a.x_start > b.x_start) {
+            console.log(b.x_start);
+            return 1;
+        }
+        if (a.xstart < b.x_start) {
+            console.log(a.x_start);
+            return -1;
+        }
+        return 0;
+    } else {
+        return a.y_start - b.y_start;
     }
-    if (a.xstart < b.x_start) {
-        return -1;
-    }
-    return 0;
-    //return 0;
-    //}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function distForm(a, b) {
